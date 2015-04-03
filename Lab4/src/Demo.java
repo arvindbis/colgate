29e56f4872a37e84344a4457bf43adf07d72d728
Arvind <arvind.kumar@questionpro.com>
2015-04-03 09:23:48 +0530

    Basic UI

--- Lab4/src/Demo.java
+++ Lab4/src/Demo.java
@@ -1,7 +1,8 @@
+import view.Luncher;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("hello Marwa, your Repository is Now active.");
-        System.out.println("nice to meet you");
+        Luncher myApplication  = new Luncher("Lab4");
+
     }
 }
--- Lab4/src/view/Luncher.java
+++ Lab4/src/view/Luncher.java
@@ -1,7 +1,46 @@
 package view;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 /**
  * Created by QP-DEV on 4/3/2015.
  */
-public class Luncher {
+public class Luncher extends JFrame {
+
+    private JPanel imageHolder,perspectiveHolder1,getPerspectiveHolder2;
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenuItem save;
+    public Luncher(String name){
+        initalizeView(name);
+    }
+
+    private void initalizeView(String name){
+        new JFrame();
+        this.setTitle(name);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+        this.setJMenuBar(menuBar);
+        save = new JMenuItem("save");
+        save.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                savePerspectives(evt);
+            }
+        });
+        fileMenu.add(save);
+    }
+
+    private void savePerspectives(ActionEvent evt) {
+        if("save".equals(evt.getActionCommand())){
+            System.out.println("Save Button is pressed");
+        }
+    }
+
 }
